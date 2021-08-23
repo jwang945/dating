06f9a63c147b44fa677b10479fb43e4d8ce3549c
Jet-Wang <jwang945@gatech.edu>
2021-08-23 18:18:57 -0400

    add body component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import NavBar from "./components/navBar";
+import Body from "./components/body";
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Body />
     </div>
   );
 }
---
+++ src/components/body.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+class Body extends Component {
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        <p>Placehoder</p>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Body;
