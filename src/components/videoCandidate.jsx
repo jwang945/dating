376155b62538152541bd19b99a1a5dc239c49a8b
Jet-Wang <jwang945@gatech.edu>
2021-08-23 19:37:14 -0400

    add overlayDescription text

--- src/components/videoCandidate.jsx
+++ src/components/videoCandidate.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 class VideoCandidate extends Component {
   state = {};
   render() {
-    const overlayTextStyle = {
+    const overlayNameStyle = {
       position: "absolute",
       top: "70%",
       left: "39%",
@@ -10,11 +10,28 @@ class VideoCandidate extends Component {
       fontSize: 25,
       color: "white"
     };
+    const overlayDescriptionStyle = {
+      position: "absolute",
+      top: "74%",
+      left: "39%",
+      zIndex: 1,
+      fontSize: 16,
+      color: "white",
+      wordWrap: "break-word",
+      width: 300,
+      textAlign: "left"
+    };
     return (
       <React.Fragment>
         {this.props.children}
-        <div style={overlayTextStyle} class="overlayText">
-          <p>Overlay Text</p>
+        <div style={overlayNameStyle} class="overlayName">
+          <p>Nala Cat</p>
+        </div>
+        <div style={overlayDescriptionStyle}>
+          <p>
+            I like to eat frogs 😍 and I somehow have hairballs and i only date
+            badboys 9/10 only
+          </p>
         </div>
       </React.Fragment>
     );
