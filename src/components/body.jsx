d080c51f0125bf100c9b11355e7208fe0b7cdc6e
Jet-Wang <jwang945@gatech.edu>
2021-08-23 18:24:40 -0400

    add match and video candidate components

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import MatchCandidate from "./matchCandidate";
 class Body extends Component {
   state = {};
   render() {
     return (
       <React.Fragment>
         <p>Placehoder</p>
+        <MatchCandidate />
       </React.Fragment>
     );
   }
---
+++ src/components/matchCandidate.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import VideoCandidate from "./videoCandidate";
+class MatchCandidate extends Component {
+  state = {};
+  render() {
+    return <React.Fragment></React.Fragment>;
+  }
+}
+
+export default MatchCandidate;
