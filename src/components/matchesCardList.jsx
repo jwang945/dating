e955d8bb31ba0f433ad5186c244ef6cc65072727
Jet-Wang <jwang945@gatech.edu>
2021-08-24 22:28:19 -0400

    add more matches examples

--- src/components/matchesCardList.jsx
+++ src/components/matchesCardList.jsx
@@ -2,6 +2,14 @@ import React, { Component } from "react";
 import MatchCard from "./matchCard";
 import bojangleIcon from "./../matchPictures/bojangleIcon.png";
 import pandaIcon from "./../matchPictures/pandaIcon.png";
+import burgerKingIcon from "./../matchPictures/burgerKingIcon.png";
+import cfaIcon from "./../matchPictures/cfaIcon.png";
+import dairyQueenIcon from "./../matchPictures/dairyQueenIcon.png";
+import jackIcon from "./../matchPictures/jackIcon.png";
+import mcdonaldsIcon from "./../matchPictures/mcdonaldsIcon.png";
+import popeyesIcon from "./../matchPictures/popeyesIcon.png";
+import shakeShackIcon from "./../matchPictures/shakeShackIcon.png";
+import wendysIcon from "./../matchPictures/wendysIcon.png";
 class MatchesCardList extends Component {
   state = {
     matches: [
@@ -16,6 +24,54 @@ class MatchesCardList extends Component {
         name: "Panda Express",
         latestMessage: "hey sexy",
         icon: pandaIcon
+      },
+      {
+        uuid: "003",
+        name: "Burger King",
+        latestMessage: "example last message",
+        icon: burgerKingIcon
+      },
+      {
+        uuid: "004",
+        name: "Chick-fil-a",
+        latestMessage: "example last message",
+        icon: cfaIcon
+      },
+      {
+        uuid: "005",
+        name: "Dairy Queen",
+        latestMessage: "example last message",
+        icon: dairyQueenIcon
+      },
+      {
+        uuid: "006",
+        name: "Jack in the Box",
+        latestMessage: "example last message",
+        icon: jackIcon
+      },
+      {
+        uuid: "007",
+        name: "McDonald's",
+        latestMessage: "example last message",
+        icon: mcdonaldsIcon
+      },
+      {
+        uuid: "008",
+        name: "Popeyes",
+        latestMessage: "example last message",
+        icon: popeyesIcon
+      },
+      {
+        uuid: "009",
+        name: "Shake Shack",
+        latestMessage: "example last message",
+        icon: shakeShackIcon
+      },
+      {
+        uuid: "010",
+        name: "Wendy's",
+        latestMessage: "example last message",
+        icon: wendysIcon
       }
     ]
   };
